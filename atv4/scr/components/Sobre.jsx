9f74b6511118791c30a30f874935eedd65bab346
LaneSan <shelidacrislane@gmail.com>
2021-01-31 23:40:55 -0300

    Atv 4

---
+++ atv4/scr/components/Sobre.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default class Sobre extends Component {
+
+    render(){        
+        return(
+            <View>
+               <Text style={{fontWeight:"bold", fontSize:20}}>
+                   Sofrimento de Projeto Interface para Dispositivos Móveis.
+                   Professor tem pena de mim e me passa, por favor!
+                </Text>
+            </View>
+        )
+    }
+}
